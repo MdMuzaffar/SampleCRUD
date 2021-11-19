172e01a3d81cabc1d9ddd09e84c83206b3e21b5c
MdMuzaffar <muzaffarahmed4u@gmail.com>
2021-11-20 00:53:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { Component , useState } from "react";
+
+const App = () => {
+
+  const [inputList, setInputList] =  useState("");
+  const [items, setItems] = useState([]);
+
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value)
+  };
+   
+  const listItems = () => {
+    setItems( (oldItems ) => {
+      return [...oldItems, inputList];
+    })
+  };
+
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1> ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add Items" onChange={itemEvent}/>
+          <button onClick={listItems}> + </button>
+
+
+          <ol>
+            {items.map( (itemsval) => {
+              return <li>{itemsval}</li>
+            })}
+          </ol>
+        </div>  
+      </div>
+    </>
+  );
+};
+
+export default App;
